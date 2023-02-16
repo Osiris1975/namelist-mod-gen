1ea620835bcf5f48cba0919a38778e795818571b
Amr Abouelleil <aabouelleil@seniorlink.com>
2023-02-16 15:37:50 -0500

    localization generation

--- src/namelist_mod_gen/constants.py
+++ src/namelist_mod_gen/constants.py
@@ -3,8 +3,12 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 TEMPLATES_DIR = os.path.join(ROOT_DIR, 'templates')
 NAMELIST_TEMPLATE = 'namelist.txt'
-LOCALIZATION_TEMPLATE = 'localization.yml'
-MIL_NAMES_LOC_TEMPLATE = 'military_names_loc.yml'
+LOCALIZATION_TEMPLATE = 'name_lists_loc.yml'
+ORD_NAMES_LOC_TEMPLATE = 'ord_names_loc.yml'
 MOD_OUTPUT_DIR = os.path.join(ROOT_DIR, 'generated_mods')
-NAME_LIST_OUTPUT_DIR = os.path.join(ROOT_DIR, 'common', 'name_lists')
-LOCALIZATION_OUTPUT_DIR = os.path.join(ROOT_DIR, 'localisation', 'english')
+
+ORD_TYPES = {
+    "C": "SEQ",
+    "O": "ORD",
+    "R": "R"
+}
--- src/namelist_mod_gen/namelist_mod_gen.py
+++ src/namelist_mod_gen/namelist_mod_gen.py
@@ -7,28 +7,83 @@ from jinja2 import Environment, FileSystemLoader
 import jinja2schema
 import os
 import sys
+import re
 from pathlib import Path
+from collections import OrderedDict
 
 template_loader = FileSystemLoader(searchpath=c.TEMPLATES_DIR)
 template_env = Environment(loader=template_loader)
-namelist_template = template_env.get_template(c.NAMELIST_TEMPLATE)
 
 
-def render_namelist(args):
+def make_mod_directories(mod_name, lang):
+    dirs = {
+        "namelist": os.path.join(c.MOD_OUTPUT_DIR, mod_name, 'common', 'name_lists'),
+        "localization": os.path.join(c.MOD_OUTPUT_DIR, mod_name, 'localisation', lang)
+    }
+
+    for d in dirs.values():
+        if not os.path.exists(d):
+            os.makedirs(d)
+    return dirs
+
+
+def create_mod(args):
+    dirs = make_mod_directories(args.mod_name, 'english')
+    nl_dict, ord_dict = csv_to_dicts(args.namelist_csv)
+
+    name_list_file = os.path.join(dirs["namelist"], f"{nl_dict['namelist_id']}.txt")
+    with open(name_list_file, 'w') as file:
+        namelist_template = template_env.get_template(c.NAMELIST_TEMPLATE)
+        name_list = namelist_template.render(nl_dict)
+        file.write(name_list)
+        print(f'Namelist file written to {name_list_file}')
+
+    namelist_loc_file = os.path.join(dirs["localization"], f"{nl_dict['namelist_author'].lower()}_namelist_l_english.yml")
+    with open(namelist_loc_file, 'w') as file:
+        nl_loc_template = template_env.get_template(c.LOCALIZATION_TEMPLATE)
+        nl_loc_dict = {
+            nl_dict['namelist_id']: {
+                'id': nl_dict['namelist_id'].lower(),
+                'author': nl_dict['namelist_author'].lower(),
+                'title': nl_dict['namelist_title']
+            }
+        }
+        nl_loc = nl_loc_template.render(dict_item=[nl_loc_dict])
+        file.write(nl_loc)
+        print(f'Namelist localization file written to {name_list_file}')
+
+    ord_loc_file = os.path.join(dirs["localization"], f"name_list_{nl_dict['namelist_id'].upper()}_l_english.yml")
+    with open(ord_loc_file, 'w') as file:
+        ord_loc_template = template_env.get_template(c.ORD_NAMES_LOC_TEMPLATE)
+        ord_loc = ord_loc_template.render(dict_item=ord_dict)
+        file.write(ord_loc)
+        print(f'Ordinal namelist localization file written to {ord_loc_file}')
+
+
+def create_seq_key(key, value, id):
+    ord = re.search(r'\$\S\$', value).group().replace('$', '')
+    ord_base = "".join(key.split('_')[1:]).upper()
+    return f"{id.upper()}_{ord_base}_{c.ORD_TYPES[ord]}"
+
+
+def csv_to_dicts(namelist_csv):
     namelist_dict = {key: [] for key in get_template_variables()}
-    with open(args.namelist_csv, 'r') as file:
+    with open(namelist_csv, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
             for k, v in row.items():
                 if k in namelist_dict.keys() and len(v) > 0:
                     qv = (f'\"{v}\"' if 'namelist' not in k else v)
                     namelist_dict[k].append(qv)
-    namelist_dict2 = {key: " ".join(value) for key, value in namelist_dict.items()}
-    file_dest = os.path.join(c.NAME_LIST_OUTPUT_DIR, f"{namelist_dict2['namelist_id']}.txt")
-    with open(file_dest, 'w') as file:
-        template_string = namelist_template.render(namelist_dict2)
-        file.write(template_string)
-        print(f'Namelist file written to {file_dest}')
+
+    processed_dict = {key: " ".join(value) for key, value in namelist_dict.items()}
+    ord_dict = OrderedDict()
+    for k, v in processed_dict.items():
+        if "$" in v:
+            seq_key = create_seq_key(k, v, namelist_dict['namelist_id'][0])
+            ord_dict[seq_key] = v
+            processed_dict[k] = seq_key
+    return processed_dict, ord_dict
 
 
 def get_template_variables():
@@ -57,7 +112,7 @@ def main():
     if args.dump_csv_template:
         csv_template(args)
     else:
-        render_namelist(args)
+        create_mod(args)
 
 
 parser = argparse.ArgumentParser(
@@ -65,6 +120,7 @@ parser = argparse.ArgumentParser(
     usage='namelist_generator.py -c [NAMELIST_FILE]',
     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument('-c', '--namelist_csv', help="path to the namelist csv file", required=False)
+parser.add_argument('-m', '--mod_name', help="name to use for the generated mod", required=False)
 parser.add_argument('-d', '--dump_csv_template', help='dump a blank csv with namelist headers with the specified name',
                     required=False)
 
